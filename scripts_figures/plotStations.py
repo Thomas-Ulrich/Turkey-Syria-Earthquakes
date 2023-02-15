d8e75c904e90445f729dbbf689a65640da51d45d
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2023-02-15 16:23:12 +0100

    plot Station script

---
+++ scripts_figures/plotStations.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import obspy
+import os
+import shapefile as shp
+import pandas as pd
+
+
+def SetupMap(ax, gridlines_left=True):
+    # Setup the map with cartopy
+    ax.set_extent([35.5, 39.5, 35.8, 38.2], crs=ccrs.PlateCarree())
+    scale = "10m"
+    ax.add_feature(cfeature.LAND.with_scale(scale))
+    ax.add_feature(cfeature.OCEAN.with_scale(scale))
+    ax.add_feature(cfeature.COASTLINE.with_scale(scale))
+    ax.add_feature(cfeature.BORDERS.with_scale(scale), linestyle=":")
+    locs = np.arange(-180, 180, 2.0)
+    gl = ax.gridlines(draw_labels=True, ylocs=locs, xlocs=locs)
+    gl.right_labels = False
+    gl.top_labels = False
+    gl.left_labels = gridlines_left
+
+
+fig = plt.figure()
+fig.set_size_inches(10, 10)
+ax = []
+ax.append(fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree()))
+SetupMap(ax[0])
+
+stations2plot = [
+    "0214",
+    "0208",
+    "4611",
+    "4632",
+    "4611",
+    "4625",
+    "4616",
+    "2718",
+    "3138",
+    "3139",
+    "3141",
+    "3140",
+]
+
+
+for fault in ["main_fault_segmented", "fault_near_hypo"]:
+    fn = f"/home/ulrich/work/Mw_78_Turkey/{fault}.shp"
+    sf = shp.Reader(fn)
+    for sr in sf.shapeRecords():
+        listx = []
+        listy = []
+        for xNew, yNew in sr.shape.points:
+            listx.append(xNew)
+            listy.append(yNew)
+        plt.plot(listx, listy, "k")
+
+
+fn = "/home/ulrich/work/Mw_78_Turkey/Turkey-Syria-Earthquakes/ThirdParty/stations.csv"
+cols = ["Code", "Longitude", "Latitude"]
+df = pd.read_csv(fn)
+
+for index, row in df.iterrows():
+    is_inside = (35.5 <= row["Longitude"] <= 39.5) and (35.8 <= row["Latitude"] <= 38.2)
+    c = "b" if row["Code"] in stations2plot else "k"
+    if row["DeviceCode"] == "N" and is_inside:
+        plt.scatter(
+            row["Longitude"],
+            row["Latitude"],
+            marker="v",
+            facecolors="none",
+            edgecolors=c,
+        )
+        plt.text(row["Longitude"] + 0.002, row["Latitude"], row["Code"], size=8)
+
+df["waveform_to_plot"] = [row["Code"] in stations2plot for index, row in df.iterrows()]
+df = df.loc[df["waveform_to_plot"]]
+for index, row in df.iterrows():
+    is_inside = (35.5 <= row["Longitude"] <= 39.5) and (35.8 <= row["Latitude"] <= 38.2)
+    if row["DeviceCode"] == "N" and is_inside:
+        plt.scatter(
+            row["Longitude"],
+            row["Latitude"],
+            marker="v",
+            facecolors="none",
+            edgecolors="b",
+        )
+        plt.text(
+            row["Longitude"] + 0.002, row["Latitude"], row["Code"], size=8, color="b"
+        )
