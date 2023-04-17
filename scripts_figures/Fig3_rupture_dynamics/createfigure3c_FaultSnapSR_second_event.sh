0601cf017d6c9fb9156be56e3dc1ade2e86c50e5
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2023-04-17 12:00:54 -0400

    update Fig3

--- scripts_figures/Fig3_rupture_dynamics/createfigure3c_FaultSnapSR_second_event.sh
+++ scripts_figures/Fig3_rupture_dynamics/createfigure3c_FaultSnapSR_second_event.sh
@@ -3,7 +3,7 @@ myfold=$1
 
 outFile="figureSR_second_event.png"
 filelist=""
-for k in 102 108 116 120 124 128
+for k in 152 158 166 170 174 178
 do
     python ~/TuSeisSolScripts/displayh5vtk/displayUnstructuredVtk.py $1-fault.xdmf --Data SR --pvcc ../pvcc/SEviewTurkey_flatter.pvcc --at_time $k --BoundaryEdges --zoom 1.0 --crange 0 8 --colorScale viridis_r0 --winSize 1600 450  --OSR --oneDtMem --output trash_me$k --opa 0.99 --clip "0 60e3 0 0.7682212795973759 -1.0 0"
    filelist="$filelist output/trash_me$k.png"
--- scripts_figures/Fig3_rupture_dynamics/createfigure3d_stress_intersection.sh
+++ scripts_figures/Fig3_rupture_dynamics/createfigure3d_stress_intersection.sh
@@ -3,12 +3,12 @@ myfold=$1
 
 outFile="figure_tractions_intersection.png"
 filelist=""
-for k in 12.5 17.0
+for k in 12 15.0
 do
     python ~/TuSeisSolScripts/displayh5vtk/displayUnstructuredVtk.py $1-fault.xdmf --Data T_s --pvcc ../pvcc/focus_intersection.pvcc --at_time $k --BoundaryEdges --zoom 5.0 --crange " -1e7" 1e7 --colorScale roma --winSize 800 800  --OSR --oneDtMem --output trash_me_Ts$k --clip "0 60e3 0 -0.7682212795973759 1.0 0"
    filelist="$filelist output/trash_me_Ts$k.png"
 done
-for k in 12.5 17.0
+for k in 12 15.0
 do
     python ~/TuSeisSolScripts/displayh5vtk/displayUnstructuredVtk.py $1-fault.xdmf --Data P_n --pvcc ../pvcc/focus_intersection.pvcc --at_time $k --BoundaryEdges --zoom 5.0 --crange " -2e7" 2e7 --colorScale vik --winSize 800 800  --OSR --oneDtMem --output trash_me_Pn$k --clip "0 60e3 0 -0.7682212795973759 1.0 0"
    filelist="$filelist output/trash_me_Pn$k.png"
