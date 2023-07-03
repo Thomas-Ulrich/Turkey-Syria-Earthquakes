fix RMS calculation

scripts_figures/Fig2_comparison_geodetics/compare_GPS.py
@@ -266,16 +266,15 @@ ax[0].text(
 )
 
 # Compute RMS
-res1 = ui - ew
-res2 = vi - ns
-res3 = wi - dz
-N = max(np.shape(res1))
-rms1 = np.sqrt(np.nansum(res1**2)/N);
-rms2 = np.sqrt(np.nansum(res2**2)/N);
-rms3 = np.sqrt(np.nansum(res3**2)/N);
+def nanrms(x, axis=None):
+    return np.sqrt(np.nanmean(x**2, axis=axis))
+
+rms1 = nanrms(ui - ew)
+rms2 = nanrms(vi - ns)
+rms3 = nanrms(wi - dz)
+
 rmstot = np.sqrt(rms1**2 + rms2**2 + rms3**2)
 print('RMS GPS:',np.round(rmstot,5),'m')
-
 
 # Write .png file
 fn = f"output/comp_GPS_event_{event}_comp_{arg.component[0]}.png"